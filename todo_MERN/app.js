00cd0863fe598229acddafc6e919ebcb2136927d
louvkrishnaupadhyay <louv2k@gmail.com>
2026-03-31 12:00:30 +0530

    basic REST API

--- todo_MERN/app.js
+++ todo_MERN/app.js
@@ -2,6 +2,23 @@ const express = require('express');
 
 const app = express();
 
+//POST
+app.post('/', (req, res) => {
+    res.send("post method called");
+});
+//GET
+app.get('/',(req,res)=>{
+    res.send("get method called");
+});
+//PUT
+app.put('/',(req,res)=>{
+    res.send("update method called");
+});
+//DELETE
+app.delete('/',(req,res)=>{
+    res.send("delete method called");
+});
+
 app.listen(3000, (err) =>{
     if(err){
         console.log(err);
