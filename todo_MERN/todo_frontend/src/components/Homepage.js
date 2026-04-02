c22ccf4ba9be606ebe0b4a4cc9ff2fbc9a6286ae
louvkrishnaupadhyay <louv2k@gmail.com>
2026-04-02 12:29:32 +0530

    adding the css for homepage

--- todo_MERN/todo_frontend/src/components/Homepage.js
+++ todo_MERN/todo_frontend/src/components/Homepage.js
@@ -1,6 +1,7 @@
 import React, {useEffect, useState} from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
+import './Homepagemodule.css';
 
 const Homepage = () => {
     const navigate = useNavigate();
@@ -17,34 +18,34 @@ const Homepage = () => {
     console.log("user", user);
 
     return (
-        <>
-            <button onClick={() => navigate("/Adduser")}>Adduser</button>
-            <button onClick={() => navigate("/Edituser")}>Edituser</button>
-            <div>Homepage</div>
+  <div className="container">
+    <h1>User Dashboard</h1>
 
-            <table>
-                <thead>
-                    <tr>
-                        <th>ID</th>
-                        <th>userName</th>
-                        <th>Mobile</th>
-                        
-                    </tr>
-                </thead>
-                <tbody>
-                    {user && user.map((post)=>{
-                        return (
-                            <tr key={post._id}>
-                                <td>{post._id}</td>
-                                <td>{post.userName}</td>
-                                <td>{post.mobile}</td>
-                            </tr>
-                        )
-                    })}
-                </tbody>
-            </table>
-        </>
-    );
+    <div className="button-group">
+      <button onClick={() => navigate("/Adduser")}>Add User</button>
+      <button onClick={() => navigate("/Edituser")}>Edit User</button>
+    </div>
+
+    <table>
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>User Name</th>
+          <th>Mobile</th>
+        </tr>
+      </thead>
+      <tbody>
+        {user && user.map((post) => (
+          <tr key={post._id}>
+            <td>{post._id}</td>
+            <td>{post.userName}</td>
+            <td>{post.mobile}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  </div>
+);
 };
 
 export default Homepage;
